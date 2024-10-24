2ae0db68f395946f5a12018a87e677e7206f9228
Ryan Dudley <ryguy122000@gmail.com>
2024-10-24 01:38:05 -0400

    test

--- DatabaseProjectAPI/Program.cs
+++ DatabaseProjectAPI/Program.cs
@@ -47,11 +47,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
